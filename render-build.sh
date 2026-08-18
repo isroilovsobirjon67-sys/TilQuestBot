9009806da5636db6398595bc805d7c3b2dd12ef1
isroilovsobirjon67-sys <isroilovsobirjon67@gmail.com>
2026-08-18 19:59:41 +0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
 apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-rus
+apt-get install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-rus
+
 pip install -r requirements.txt
